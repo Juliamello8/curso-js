58794ed7147bdf37ebbfc184a27d56b50afbf006
Juliamello08 <juliainline@gmail.com>
2020-05-11 22:24:50 -0300

    Adicionando exercicio aula 14

---
+++ aula13/ambiente1.js
@@ -0,0 +1,15 @@
+//while
+var c=1;
+while (c<=500){
+    console.log(`Passo ${c}`);
+    c++;
+}
+//do while
+do{
+    console.log(`Passo ${c}`);
+    c++;
+} while(c<=500)
+//for
+for (var c=1; c<=500 ; c++){
+    console.log(`Passo ${c}`);
+}
---
+++ aula14ex/Ex16/script.js
@@ -0,0 +1,14 @@
+function Contar(){
+    var i = document.getElementById('inicio') ;
+    var f = document.getElementById('fim');
+    var p = document.getElementById('passo');
+    if (i < f) {
+        var res = document.createElement('div')
+        window.alert('Inicio maior que Final!')
+    } else {
+        window.alert('Final maior que Inicio!')
+        // for(i ; i < f; i+p){
+
+        // }
+    }
+}
