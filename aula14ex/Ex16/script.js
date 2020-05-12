76da75d0ff18e31cd61ee45442f4442e37ebdc28
Juliamello08 <juliainline@gmail.com>
2020-05-12 14:19:49 -0300

    c

--- aula14ex/Ex16/script.js
+++ aula14ex/Ex16/script.js
@@ -1,14 +1,19 @@
 function Contar(){
-    var i = document.getElementById('inicio') ;
-    var f = document.getElementById('fim');
-    var p = document.getElementById('passo');
-    if (i < f) {
-        var res = document.createElement('div')
-        window.alert('Inicio maior que Final!')
-    } else {
-        window.alert('Final maior que Inicio!')
-        // for(i ; i < f; i+p){
+    //let a variavel só funciona nesse escopo
+    let i = document.getElementById('inicio') ;
+    let f = document.getElementById('fim');
+    let p = document.getElementById('passo');
+    let res = document.getElementById('res');
+    if (i.value.length == 0 || f.value.length == 0 || p.value.length == 0){
+        window.alert('Há campos vazios, favor conferir!');
+    }else {
+        res.innerHTML = "Contando: "
+        if (i.value < f.value) {
+            window.alert('Contagem normal!');
+            var res = document.createElement('div');
+        } else if (f.value < i.value) {
+            window.alert('Contagem regressiva!');
 
-        // }
+        }
     }
 }
