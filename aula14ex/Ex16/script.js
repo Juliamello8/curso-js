bfbd7438fa45ce5a69de0a053e4d319dceb26149
Juliamello08 <juliainline@gmail.com>
2020-05-13 09:03:35 -0300

    Adicionando alterações

--- aula14ex/Ex16/script.js
+++ aula14ex/Ex16/script.js
@@ -1,19 +1,19 @@
 function Contar(){
     //let a variavel só funciona nesse escopo
-    let i = document.getElementById('inicio') ;
-    let f = document.getElementById('fim');
-    let p = document.getElementById('passo');
+    let ini = document.getElementById('inicio') ;
+    let fim = document.getElementById('fim');
+    let pas = document.getElementById('passo');
     let res = document.getElementById('res');
-    if (i.value.length == 0 || f.value.length == 0 || p.value.length == 0){
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){
         window.alert('Há campos vazios, favor conferir!');
     }else {
         res.innerHTML = "Contando: "
-        if (i.value < f.value) {
-            window.alert('Contagem normal!');
-            var res = document.createElement('div');
-        } else if (f.value < i.value) {
-            window.alert('Contagem regressiva!');
-
+        let i = Number(ini.value);
+        let f = Number(fim.value);
+        let p = Number(pas.value);
+        
+        for(let c=1; c<=f; c+=p){
+            res.innerHTML += `${c}`;
         }
     }
 }
