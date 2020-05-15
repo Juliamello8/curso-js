561944c603ae388a1920cbc20414198330b4c581
Juliamello08 <juliainline@gmail.com>
2020-05-15 08:22:15 -0300

    Iniciando ex.17 - Tabuada

--- aula14ex/Ex16/script.js
+++ aula14ex/Ex16/script.js
@@ -6,12 +6,16 @@ function Contar(){
     let res = document.getElementById('res');
     
     if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){
-        window.alert('Há campos vazios, favor conferir!');
+        res.innerHTML = `Conta não efetuada! Há campos em branco.`
     }else {
-        res.innerHTML = "Contando: "
+        res.innerHTML = "Contando: </br> "
         let i = Number(ini.value);
         let f = Number(fim.value);
         let p = Number(pas.value);
+        if (p >= 0){
+            window.alert('Passo informado inválido! Passo considerado é: 1')
+            p=1;
+        }
         if( i < f){
             for(let c = i; c <= f; c += p){
                 res.innerHTML += ` ${c} \u{27A1}`;
--- aula14ex/Ex17/script.js
+++ aula14ex/Ex17/script.js
@@ -1,1 +1,17 @@
-alert("Ola");
+function Gerar(){
+    let num = document.getElementById('num');
+    let res = document.getElementById('res');
+
+    if (num >= 0){
+        // for (let i = 0; i <= 10; i++){
+        //     let result = num * i; 
+        //     res.innerHTML = `${result}`;
+        // }
+        window.alert("Tudo ok!")
+    } else if (num < 0){
+        window.alert("Não é possível calcular número negativo!");
+    } else if (num == NaN || num == null){
+        window.alert("Favor preencher campo!");
+    }
+
+}
