15d7989915d908fa6b436fe423c1c33bf4f604e4
Juliamello08 <juliainline@gmail.com>
2020-05-24 21:55:26 -0300

    Final curso JavaScript CursoemVideo

--- aula16ex/ex18/script.js
+++ aula16ex/ex18/script.js
@@ -21,8 +21,41 @@ function inLista(n, l){
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, valores)){
-        window.alert("Deu!")
+        valores.push(Number(num.value)); // ppush add elemento no vetor
+        let item = document.createElement('option');
+        item.text = `Valor ${num.value} adicionado!`;
+        lista.appendChild(item);
+        res.innerHTML = '';
     }else{
-        window.alert("Valor inválido ou já encontrado na lista!")
+        window.alert("Valor inválido ou já inserido na lista!")
+    }
+    num.value = '';
+    num.focus(); //apaga o valor no input automaticamente
+}
+
+function finalizar() {
+    if (valores.length == 0){
+        window.alert("Adicione valores antes de finalizar!");
+    } else {
+        let tot = valores.length;
+        let maior = valores[0];
+        let menor = valores[0];
+        let soma = 0;
+        for (let pos in valores){
+            soma += valores[pos];
+            if (valores[pos] > maior){
+                maior = valores[pos];
+            }
+            if (valores[pos] < menor){
+                menor = valores[pos];
+            }
+        }
+        media = soma/tot;
+        res.innerHTML = '';
+        res.innerHTML += `Total de valores adicionados: ${tot}`
+        res.innerHTML += `</br>O menor valor é: ${menor}`;
+        res.innerHTML += `</br>O maior valor é: ${maior}`;
+        res.innerHTML += `</br>A soma dos valores é: ${soma}`
+        res.innerHTML += `</br>A média dos valores é: ${media}`
     }
 }
